8d7444e9f98f03c9f449c7942b9fda79a35f48d1
casele <casele@vsp.com>
2013-03-11 14:29:57 -0700

    latest

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-stash-jenkins-hooks
-===================
-
-Stash Hook for Jenkins
+# Stash Plugin to Notify Jenkins
+A basic post-receive hook that notifies Jenkins when changes are pushed to Stash.  This plugin requires the Git Plugin to be installed on your Jenkins server (https://wiki.jenkins-ci.org/display/JENKINS/Git+Plugin)
+To specifiy a default Jenkins URL, startup Stash with JAVA_OPTS="-Djenkins.url=http://foo.com"
--- src/main/java/com/nektos/stash/jenkins/RepositoryHook.java
+++ src/main/java/com/nektos/stash/jenkins/RepositoryHook.java
@@ -1,5 +1,7 @@
 package com.nektos.stash.jenkins;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Collection;
@@ -8,29 +10,56 @@ import com.atlassian.stash.hook.repository.AsyncPostReceiveRepositoryHook;
 import com.atlassian.stash.hook.repository.RepositoryHookContext;
 import com.atlassian.stash.nav.NavBuilder;
 import com.atlassian.stash.repository.RefChange;
+import com.atlassian.stash.repository.RefChangeType;
 import com.atlassian.stash.repository.Repository;
 import com.atlassian.stash.setting.RepositorySettingsValidator;
 import com.atlassian.stash.setting.Settings;
 import com.atlassian.stash.setting.SettingsValidationErrors;
+import com.atlassian.stash.ssh.api.SshCloneUrlResolver;
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 
 public class RepositoryHook implements AsyncPostReceiveRepositoryHook, RepositorySettingsValidator {
 
 	private final NavBuilder navBuilder;
+	private final SshCloneUrlResolver sshResolver;
+    private static final String REFS_HEADS = "refs/heads/";
 
-	public RepositoryHook(NavBuilder navBuilder) {
+
+	public RepositoryHook(NavBuilder navBuilder, SshCloneUrlResolver sshResolver) {
 		this.navBuilder = navBuilder;
+		this.sshResolver = sshResolver;
 	}
 
+	
     /**
      * Connects to a configured URL to notify of all changes.
      */
     @Override
     public void postReceive(RepositoryHookContext context, Collection<RefChange> refChanges) {
+    	boolean includeBranch = (!context.getSettings().getBoolean("no_branch", false));
+    	boolean useSSH = (sshResolver!=null && !context.getSettings().getBoolean("use_http", false));
         String url = context.getSettings().getString("jenkins_url");
+        if(url == null) {
+        	url = System.getProperty("jenkins.url");
+        }
         if (url != null) {
             try {
-            	String scmUrl = navBuilder.repo(context.getRepository()).clone("ssh").buildAbsolute();
-				url += "/git/notifyCommit?url="+URLEncoder.encode(scmUrl,"UTF-8");
+
+            	String scmUrl;
+            	if(useSSH) {
+            		URI uri = new URI(sshResolver.getCloneUrl(context.getRepository()));
+            	    scmUrl=new URI(uri.getScheme(), null, uri.getHost(), uri.getPort(), uri.getPath(), uri.getQuery(), uri.getFragment()).toASCIIString();
+            	} else {
+            		scmUrl=navBuilder.repo(context.getRepository()).clone("git").buildAbsoluteWithoutUsername();
+            	}
+				url += "/git/notifyCommit?url="+urlEncode(scmUrl);
+            	if(includeBranch) {
+	                url += "&branches="+urlEncode(Joiner.on(",").join(getUpdateBranches(refChanges)));
+            	}
+				System.out.println("Notifying '"+url+"'");
                 new URL(url).openConnection().getInputStream().close();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -40,8 +69,33 @@ public class RepositoryHook implements AsyncPostReceiveRepositoryHook, Repositor
 
     @Override
     public void validate(Settings settings, SettingsValidationErrors errors, Repository repository) {
-        if (settings.getString("jenkins_url", "").isEmpty()) {
+        if (settings.getString("jenkins_url", System.getProperty("jenkins.url")).isEmpty()) {
             errors.addFieldError("jenkins_url", "Jenkins URL field is blank, please supply one");
         }
     }
+    
+    private static String urlEncode(String string) {
+        try {
+                return URLEncoder.encode(string, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+        }
+}
+    
+    private Iterable<String> getUpdateBranches(Collection<RefChange> refChanges) {
+        return Iterables.transform(Iterables.filter(refChanges, new Predicate<RefChange>() {
+                @Override
+                public boolean apply(RefChange input) {
+                        // We only care about non-deleted branches
+                        return input.getType() != RefChangeType.DELETE && input.getRefId().startsWith(REFS_HEADS);
+                }
+        }), new Function<RefChange, String>() {
+                @Override
+                public String apply(RefChange input) {
+                        // Not 100% sure whether this is _just_ branch or is full ref?
+                        return input.getRefId().replace(REFS_HEADS, "");
+                }
+        });
+}
+    
 }
